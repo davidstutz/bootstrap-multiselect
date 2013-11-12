1b3a01d1563f1c11d6fcdac122f6dc2faac61a25
David Stutz <davidstutz@web.de>
2013-11-13 00:56:12 +0100

    #181

--- js/bootstrap-multiselect.js
+++ js/bootstrap-multiselect.js
@@ -272,9 +272,9 @@
                     $option.prop('selected', false);
                 }
 
-                this.updateButtonText();
                 this.$select.change();
                 this.options.onChange($option, checked);
+                this.updateButtonText();
                 
                 if(this.options.preventInputChangeEvent) {
                     return false;
