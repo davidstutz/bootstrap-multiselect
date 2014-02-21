1e2c1f5b327d72cffb6b7c72df2ed669219e2ba5
Phil Freo <phil@philfreo.com>
2014-02-20 17:40:37 -0800

    Fix 2 more typos

--- js/bootstrap-multiselect.js
+++ js/bootstrap-multiselect.js
@@ -810,7 +810,7 @@
         },
         
         /**
-         * Update the button text and its title base don the currenty selected options.
+         * Update the button text and its title based on the currently selected options.
          */
         updateButtonText: function() {
             var options = this.getSelected();
