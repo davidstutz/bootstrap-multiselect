49085da3be9dbefd3c52e1b29ea0afd049f648ce
davidstutz <davidstutz@web.de>
2016-08-17 23:01:37 +0200

    Fixed karma config.

--- tests/karma.conf.js
+++ tests/karma.conf.js
@@ -23,7 +23,7 @@ module.exports = function(config) {
       // preprocess matching files before serving them to the browser
       // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
       preprocessors: {
-      }
+      },
 
       // test results reporter to use
       // possible values: 'dots', 'progress'
@@ -42,7 +42,6 @@ module.exports = function(config) {
 
       // enable / disable watching file and executing tests whenever any file changes
       autoWatch: true,
-
 
       // start these browsers
       // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
