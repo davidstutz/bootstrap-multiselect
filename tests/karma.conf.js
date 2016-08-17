af897b7f81f67c31a4b72b5c0d606fd34531035c
davidstutz <davidstutz@web.de>
2016-08-17 23:19:04 +0200

    Still workingin jquery problem ...

--- tests/karma.conf.js
+++ tests/karma.conf.js
@@ -9,7 +9,7 @@ module.exports = function(config) {
 
       // frameworks to use
       // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-      frameworks: ['jasmine-jquery', 'jasmine'],
+      frameworks: ['jquery-2.1.1', 'jasmine-jquery', 'jasmine'],
 
       // list of files / patterns to load in the browser
       files: [
