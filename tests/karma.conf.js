2a3b0f66b7e78fe4931f835084090b640bd5d216
davidstutz <davidstutz@web.de>
2016-08-17 22:25:31 +0200

    Another try ...

--- tests/karma.conf.js
+++ tests/karma.conf.js
@@ -1,79 +1,85 @@
 // Karma configuration
-// Generated on Wed Aug 17 2016 00:01:09 GMT+0200 (Mitteleuropäische Sommerzeit)
+// Generated on Mon Jul 18 2016 14:06:44 GMT-0500 (Central Daylight Time (Mexico))
 
 module.exports = function(config) {
-    var configuration = {
 
-      // base path that will be used to resolve all patterns (eg. files, exclude)
-      basePath: '',
-
-
-      // frameworks to use
-      // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-      frameworks: ['jasmine'],
+  var configuration = {
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
 
 
-      // list of files / patterns to load in the browser
-      files: [
-        '*.js'
-      ],
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['jasmine'],
 
 
-      // list of files to exclude
-      exclude: [
-      ],
+    // list of files / patterns to load in the browser
+    files: [
+      '**/*.test.js'
+    ],
 
 
-      // preprocess matching files before serving them to the browser
-      // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-      preprocessors: {
-      },
+    // list of files to exclude
+    exclude: [
+    ],
 
 
-      // test results reporter to use
-      // possible values: 'dots', 'progress'
-      // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-      reporters: ['progress'],
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+    },
 
 
-      // web server port
-      port: 9876,
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress'],
 
 
-      // enable / disable colors in the output (reporters and logs)
-      colors: true,
+    // web server port
+    port: 9876,
 
 
-      // level of logging
-      // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-      logLevel: config.LOG_INFO,
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
 
 
-      // enable / disable watching file and executing tests whenever any file changes
-      autoWatch: true,
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
 
 
-      // start these browsers
-      // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-      browsers: ['Chrome', 'Firefox', 'Opera', 'IE'],
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
 
-      // e.g see https://swizec.com/blog/how-to-run-javascript-tests-in-chrome-on-travis/swizec/6647
-      customLaunchers: {
-        Chrome_travis_ci: {
-          base: 'Chrome',
-          flags: ['--no-sandbox']
-        }
-      },
 
-      // Continuous Integration mode
-      // if true, Karma captures browsers, runs the tests and exits
-      singleRun: false,
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['Chrome'],
 
-      // Concurrency level
-      // how many browser should be started simultaneous
-      concurrency: Infinity
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity
   };
+
+  // Change some config (Chrome to Chrome Canary) if Running on TravisCI.
+  // See: http://stackoverflow.com/questions/19255976/how-to-make-travis-execute-angular-tests-on-chrome-please-set-env-variable-chr
+  if(process.env.TRAVIS){
+    configuration.browsers = ['Chrome_travis_ci'];
+    configuration.singleRun = true;
+  }
 
 
   config.set(configuration);
-}
+};
