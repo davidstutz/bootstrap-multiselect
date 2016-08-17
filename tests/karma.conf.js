45f76f8afe099c21924193b41548516ef4f32049
davidstutz <davidstutz@web.de>
2016-08-17 23:25:11 +0200

    Jquery ...

--- tests/karma.conf.js
+++ tests/karma.conf.js
@@ -9,7 +9,7 @@ module.exports = function(config) {
 
       // frameworks to use
       // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-      frameworks: ['jquery', 'jasmine-jquery', 'jasmine'],
+      frameworks: ['karma-jquery', 'jasmine-jquery', 'jasmine'],
 
       // list of files / patterns to load in the browser
       files: [
