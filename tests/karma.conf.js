198b67e2cce86e906ab6b862caaa08fc560d5b4a
davidstutz <davidstutz@web.de>
2016-08-17 23:07:53 +0200

    Missing jQuery for Karma.

--- tests/karma.conf.js
+++ tests/karma.conf.js
@@ -9,7 +9,7 @@ module.exports = function(config) {
 
       // frameworks to use
       // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-      frameworks: ['jasmine'],
+      frameworks: ['jasmine-jquery', 'jasmine'],
 
       // list of files / patterns to load in the browser
       files: [
