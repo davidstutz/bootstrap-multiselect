4fb42860323acbda09111ce712ecaa66bcdae27b
Andrew Durber <aidurber@gmail.com>
2016-09-22 13:03:52 +0100

    Revert "Merge remote-tracking branch 'refs/remotes/origin/master' into advanced-templates"
    This reverts commit 71693a94adb7e277b0c6d7b9ca8127b4e4931442, reversing
    changes made to 3383d8680fb5e5867ef6a2762ac5f8e8037ddab8.

--- dist/js/bootstrap-multiselect.js
+++ dist/js/bootstrap-multiselect.js
@@ -444,9 +444,7 @@
                 li: '<li><a tabindex="0"><label></label></a></li>',
                 divider: '<li class="multiselect-item divider"></li>',
                 liGroup: '<li class="multiselect-item multiselect-group"><label></label></li>'
-            },
-            hideInputButton: false
-
+            }
         },
 
         constructor: Multiselect,
@@ -884,11 +882,7 @@
             if (name) {
                 $checkbox.attr('name', name);
             }
-          
-           if(this.options.hideInputButton){
-                $checkbox.css("display", "none");
-                $label.css("padding-left", "10px");
-           }
+
             $label.prepend($checkbox);
 
             var selected = $element.prop('selected') || false;
